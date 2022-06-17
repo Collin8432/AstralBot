dda2c8e9dc54b200eef727e2961351a13332795c
Astro6789 <collinmc1228@gmail.com>
2022-06-17 17:07:51 -0400

    Testing PT2

--- tests/test5.py
+++ tests/test5.py
@@ -1,5 +1,8 @@
 # Imports
 from tinydb import TinyDB, Query
+import disnake
+from disnake.ext import tasks
+import os
 
 
 
@@ -13,10 +16,30 @@ async def add_guild(guild_id):
    
 
 async def add_guild_message(guild_id, message_id):
-   db.update({"db_guild_message": message_id}, Query.guild_id == guild_id)"})
+   db.update({"db_guild_message": message_id}, Query.guild_id == guild_id)
    
    
 async def idsearch(guild_id):
    results = db.search(Query.guild_id == guild_id)
    for result in results:
       return result["guild_id"]
+   
+@tasks.loop(seconds=30)
+async def checkmessage(self):
+   guilds = self.bot.guilds
+   for guild in guilds:
+      try:
+         await add_guild(f"{guild.id}")
+      except:
+         pass
+      try:
+         await idsearch(f"{guild.id}")
+         ids=True
+      except:
+         ids=False   
+      while ids == True:
+         pass
+      while ids == False:
+         for channel in guild.text_channels:
+            ch = await self.bot.get_channel(channel.id)
+            await ch.send("message")
