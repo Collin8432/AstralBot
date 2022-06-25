73862c25f37f1dea73294a1b4dce9c7abcaf6322
Astro6789 <collinmc1228@gmail.com>
2022-06-25 16:57:05 -0400

    Update db.py

--- helpers/db.py
+++ helpers/db.py
@@ -3,7 +3,9 @@ import mysql.connector
 db = mysql.connector.connect(
   host="containers-us-west-72.railway.app",
   user="root",
-  password="PbK9zroblhTWZ0NYQxAh"
+  password="PbK9zroblhTWZ0NYQxAh",
+  port="7359",
+  database="railway"
 )
 
 cursor = db.cursor()
